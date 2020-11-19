c858b6244a67e718541e0a3dd362d4ffdacce14a
okakyo <0622okakyo@gmail.com>
2020-11-19 14:08:37 +0900

    与えられたデバイスの型をもとにモックサーバーを構築

---
+++ controller/README.md
@@ -0,0 +1,13 @@
+# Controller(Clean Architecture の "Infrastructure 層") 
+
+## 概要
+URLを管理するディレクトリ
+
+## ルーターの一覧
+
+```
+
+```
+
+## テストについて
+
--- main.py
+++ main.py
@@ -1,48 +1,58 @@
 from fastapi import FastAPI,Header
 import uvicorn
 import RPi.GPIO as GPIO
-
-
-# 以下:ここに実装デバイス接続を行う(Global 変数)
-
-
-SENSOR1-OUTPUT = 10
-SENSOR1-INPUT =11 
-
-
-SENSOR2-OUTPUT = 12
-SENSOR2-INPUT =13
-
-
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(15,GPIO.OUT)
-GPIO.setup(2,GPIO.IN)
-
+import asyncio
+import datetime
 
 app = FastAPI()
-
-
-
 
 @app.get("/")
 async def hello():
   return  "Hello World"
 
-
 # ここにIoT データを送信するための実装を行う
 
-@app.post("/sensor/check")
+
+"""
+PSoC デバイスからデータを取得し、送信する
+
+
+@return {
+  raspi_device_id:string,
+  user_id:string,
+  datetime: string,
+  data:{
+    psoc_device_id_key: DeviceData,
+  }
+
+"""
+@app.post("/sensors/check")
 async def send_info(): 
-  cultivation_id="0123"
+  
+  cultivation_id=0
+
+  pressureData = 300
+  humidityData = 32
+  temperatureData = 50
+  illuminanceData = 40
+  solidMoistureData = 35
+  nowDatetime = datetime.datetime.now()
+
   return {
-      "cultivation_id":cultivation_id,    
-      "pressure":"101300",    
-      "hunudity":67,    
-      "temperature":"32",    
-      "illuminance":40,
-      "soil_moisture":50,
+    "raspi_device_id":"s-5u2",
+    "user_id":"start",
+    "datetime":nowDatetime,
+    "data":{
+        "psoc_device_id-0":{
+        "cultivation_id": cultivation_id,
+        "pressure":pressureData,
+        "humidity":humidityData,
+        "illuminance":illuminanceData,
+        "solid_moisture":solidMoistureData,
+        }
+      }
     }
+
 
 if __name__=="__main__":
     uvicorn.run(app)
