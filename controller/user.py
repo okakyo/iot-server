b45b1aef73501dae484ac4293152ed8ebb001d7d
=okakyo <=0622okakyo@gmail.com>
2020-11-24 03:52:55 +0900

    サーバーを大まかに構築中

--- controller/user.py
+++ controller/user.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
+from bluetooth.ble import *
 
 router= APIRouter()
 
 @router.get("/")
-
+async def chat():
+    return ""
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI,Header
 import uvicorn
-import RPi.GPIO as GPIO
 import asyncio
 import datetime
+
 
 app = FastAPI()
 
@@ -26,32 +26,11 @@ PSoC デバイスからデータを取得し、送信する
   }
 
 """
+
+
 @app.post("/sensors/check")
-async def send_info(): 
-  
-  cultivation_id=0
-
-  pressureData = 300
-  humidityData = 32
-  temperatureData = 50
-  illuminanceData = 40
-  solidMoistureData = 35
-  nowDatetime = datetime.datetime.now()
-
-  return {
-    "raspi_device_id":"s-5u2",
-    "user_id":"start",
-    "datetime":nowDatetime,
-    "data":{
-        "psoc_device_id-0":{
-        "cultivation_id": cultivation_id,
-        "pressure":pressureData,
-        "humidity":humidityData,
-        "illuminance":illuminanceData,
-        "solid_moisture":solidMoistureData,
-        }
-      }
-    }
+async def send_info():
+  return ""
 
 
 if __name__=="__main__":
