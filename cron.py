cc765ca882bb291ec154eb263672ebed07710887
okakyo <0622okakyo@gmail.com>
2020-11-23 23:00:24 +0900

    取得したデータをパースするように実装

--- cron.py
+++ cron.py
@@ -6,26 +6,43 @@ devadr = "30:ae:a4:ee:8d:66"   # 実際にはmicro:bit のアドレスを記述
 
 exflag = False
 
+pressure=""
+temperature=""
+illuminance=""
+solidMoisture=""
+
+
 class MyDelegate(bluepy.btle.DefaultDelegate):
     def __init__(self, params):
         bluepy.btle.DefaultDelegate.__init__(self)
 
     def handleNotification(self, cHandle, data):
-        global exflag
-        print(data)
+        global pressure, temperature,illuminance, solidMoisture
+        data = str(data).replace("'","")
+        if(cHandle ==42):
+          pressure =data.split(":")[1]
+        elif(cHandle ==45):
+          temperature =data.split(":")[1]
+        elif(cHandle ==47):
+          illuminance =data.split(":")[1]
+        elif(cHandle ==49):
+          solidMoisture =data.split(":")[1]
 
 def main():
     peri = bluepy.btle.Peripheral()
     peri.connect(devadr, bluepy.btle.ADDR_TYPE_PUBLIC)
     peri.withDelegate(MyDelegate(bluepy.btle.DefaultDelegate))
 
-    # ボタン notify を要求
-    peri.writeCharacteristic(HANDLE_A_BUTTON + 1, b'\x01\x00')
-
-    print( "Notification を待機。A or B ボタン長押しでプログラム終了")
-    while exflag == False:
+    
+    while(pressure=="" or temperature=="" or illuminance =="" or solidMoisture==""):
         if peri.waitForNotifications(1.0):
             continue
+    print("全データを取得")
+    print(pressure)
+    print(temperature)
+    print(illuminance)
+    print(solidMoisture)
+
     peri.disconnect()
 
 if __name__ == "__main__":
