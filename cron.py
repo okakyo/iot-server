baf8ba947cfa1440583a6c1776328dbf2c9f2b78
okakyo <0622okakyo@gmail.com>
2020-11-26 01:15:49 +0900

    ハッカソンでデモンストレーションできるまでの状態でほぼ完成

--- cron.py
+++ cron.py
@@ -4,6 +4,9 @@ import os,sys
 import firebase_admin
 from firebase_admin import db
 from plugins import FirebasePlugin
+import datetime
+
+SERVICE_UUID ="4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 devadr = "30:ae:a4:ee:8d:66"   # 実際にはmicro:bit のアドレスを記述
 BASE_URL = "https://example.com"
 
@@ -15,7 +18,7 @@ solidMoisture=""
 firebase_admin.initialize_app(FirebasePlugin,{
   "databaseURL": "https://speak-vegetable.firebaseio.com"
 })
-deviceDb = db.reference("/device_data")
+device_db = db.reference("/device_data")
 
 # BLE の通信方法: Notify 
 # データ受信方法:  devKey:value
@@ -27,7 +30,7 @@ class MyDelegate(bluepy.btle.DefaultDelegate):
 
     def handleNotification(self, cHandle, data):
         global pressure, temperature,illuminance, solidMoisture
-        data = data.decode(encoding='utf-8').replace("'","").split(":")
+        data = data.decode(encoding='utf-8').split(":")
         if(data[0] =="pressure"):
           pressure =data[1]
         elif(data[0]=="temp"):
@@ -38,24 +41,56 @@ class MyDelegate(bluepy.btle.DefaultDelegate):
           solidMoisture =data[1]
 
 def main():
-    peri = bluepy.btle.Peripheral()
-    charas = peri.getCharacteristics()
-    peri.connect(devadr, bluepy.btle.ADDR_TYPE_PUBLIC)
-    peri.withDelegate(MyDelegate(bluepy.btle.DefaultDelegate))
-
+    try:
+      peri = bluepy.btle.Peripheral()
+      peri.connect(devadr, bluepy.btle.ADDR_TYPE_PUBLIC)
+      peri.withDelegate(MyDelegate(bluepy.btle.DefaultDelegate))
+      
+      nowDatetime = datetime.datetime.now()
+      createdAt = nowDatetime.strftime('%Y/%m/%d-%H:%M:%S')
+      while(pressure=="" or temperature=="" or illuminance =="" or solidMoisture==""):
+          if peri.waitForNotifications(1.0):
+              continue
+      
+    except Error as e:
+      logging.error(e)
+      peri.disconnect()
     
-    while(pressure=="" or temperature=="" or illuminance =="" or solidMoisture==""):
-        if peri.waitForNotifications(1.0):
-            continue
+    peri.disconnect()
+    print(nowDatetime)
     print("全データを取得")
     print("気圧:",pressure)
     print("気温:",temperature)
     print("照度:",illuminance)
     print("土壌:",solidMoisture)
-    print(charas)
-    peri.disconnect()
-    
-    
+    device_db.child(SERVICE_UUID).set({
+      "createdAt":createdAt,
+      "cultivationId":"xxxxxxxxxx",
+      "humidity":{
+        "value":"80",
+       "status":"ok"
+      },
+      "pressure":{
+       "value": pressure,
+       "status":"ok"
+       },
+      "illuminance":{
+        "value":illuminance,   
+       "status":"ng"
+      },
+      "pressure":{
+        "value":pressure,
+       "status":"ok"
+      },
+      "solid_moisture":{
+        "value":solidMoisture,  
+       "status":"ok"
+      },
+      "temperature":{
+        "value":temperature,
+       "status":"ok"
+      }
+      })
     # TODO:　ラズパイから Firebase に直接データを送信するようにして
     # getData = requests.post(BASE_URL)
     # print(getData)
